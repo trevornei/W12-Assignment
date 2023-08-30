163583063450d03a67431755e728ea3e18777c88
trevornei <117675657+trevornei@users.noreply.github.com>
2023-08-30 12:33:26 -0600

    Added comments to the app.js file.

---
+++ app.js
@@ -0,0 +1,6 @@
+// Resets submit button from refreshing the page.
+
+// Function connects data from JSON server to table element.
+
+// Function takes information from form and inputs data into db.json and table body.
+// table data-cell deletes comment.
