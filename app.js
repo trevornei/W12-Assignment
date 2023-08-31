d831df455830bff63ea5cf5e5372f29844b75061
trevornei <117675657+trevornei@users.noreply.github.com>
2023-08-31 13:31:05 -0600

    Added new font color to subtitle --> index.html

--- app.js
+++ app.js
@@ -21,7 +21,29 @@ $('#formSubmit').on('click', function(event) {
     event.preventDefault();
 })
 
-
+// Funtion that translates the page title from off the page to intended position when the page loads. 
+// 1. Select the document object.
+// 2. Use ready() method to create a function when:
+//      - The DOM is made and safe to manipulate.
+// $(document).ready(function() {
+//     // Initially set the title to be off-screen
+//     $('#h ideMe').addClass('hidden')
+//     $('#hideMe').removeClass('hidden'), {
+//         duration: 2000
+//     }
+// });
+// $(document).ready(function() {
+//     // Initially set the title to be off-screen
+//     $('#blogTitle').css({ 'transform': 'translateX(100%)' })
+//     // Animate the title to its default position
+//     $('#blogTitle').css({ 'transform': 'translateX(0%)' }, {
+//       duration: 2000, // Animation time in milliseconds
+//       step: function(now, fx) {
+//         $(this).css('-webkit-transform', now); // for Safari
+//         $(this).css('transform', now);
+//       }
+//     });
+// });
 
 // Function connects data from JSON server to table element.
 
