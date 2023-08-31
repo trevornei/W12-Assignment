05c2dc097b70fd7318f7d49ea704b51bde2ab2bc
trevornei <117675657+trevornei@users.noreply.github.com>
2023-08-31 11:25:53 -0600

    Added function that prevents page refresh on click event for button form.

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ $(window).on('scroll', function() {
     const blogTitle = $('#blogTitle');
 
     if ($(this).scrollTop() >= 20) {
-        blogTitle.addClass('hidden', 'transition-opacity');
+        blogTitle.addClass('transition-opacity');
+        blogTitle.addClass('ease-in-out');
+        blogTitle.addClass('duration-300');
+        blogTitle.addClass('hidden');
+
     } else {
         blogTitle.removeClass('hidden');
     }
 })
 
 // Resets submit button from refreshing the page.
+// 1. Select the id formSubmit with JQuery.
+// 2. Use .on() method and specify click
+// 3. Add event and preventDefault() method
+$('#formSubmit').on('click', function(event) {
+    event.preventDefault();
+})
+
 
 
 // Function connects data from JSON server to table element.
