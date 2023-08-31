54e332b42582f390f05426d7afb9b4dac37b1bdd
trevornei <117675657+trevornei@users.noreply.github.com>
2023-08-31 15:35:03 -0600

    Appended db.json data to table in index.html

--- app.js
+++ app.js
@@ -21,6 +21,72 @@ $('#formSubmit').on('click', function(event) {
     event.preventDefault();
 })
 
+
+// Create a variable that stores URL Endpoint
+const urlEndpoint = 'http://localhost:3000/userComments';
+
+// Check if variable works with a console.log() statement.
+$.get(urlEndpoint).then(data => console.log(data));
+// Success, logs the data stored inside local host 3k/userComments. 
+
+$.get(urlEndpoint).then(data => {
+    data.map(userComments => {
+        $('tbody').append(
+            $(`
+            <tr class="bg-violet-200 border-b border-violet-200 px-3 py-3">
+                <td class="m-6">${userComments.id}</td>
+                <td class="m-6">${userComments.fullName}</td>
+                <td class="m-6">${userComments.comment}</td>
+                <td class="m-6 border-slate-950 rounded-2xl bg-violet-200">
+                    <button>
+                        <p>Delete</p> 
+                    </button>
+                </td> 
+            </tr>
+            `)
+        )
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Funtion that translates the page title from off the page to intended position when the page loads. 
 // 1. Select the document object.
 // 2. Use ready() method to create a function when:
